9c4285020161db9253d851ec39c822269a86d4a6
Tim McNamara <timlmcn@gmail.com>
2017-08-08 19:14:20 -0400

    added character counter to create board

--- src/components/modals/CreateBoard.js
+++ src/components/modals/CreateBoard.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
 
 export default class CreateBoard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue : ''
+    }
+  }
+
+  _handleInput = (event) => {
+    if(event.target.value.length <= 80) {
+      this.setState({
+        inputValue : event.target.value
+      })
+    }
+  }
+
+  _submitForm = (e) => {
+    e.preventDefault();
+    console.log('hello world')
+  }
+
 
   render() {
     return (
       <div>
         <h1>Title</h1>
-        <form>
+        <form onSubmit={this._submitForm}>
           <p>Title</p>
           <input type="text" ref="title"/>
           <p>Description</p>
-          <input type="text" ref="description"/>
-          <button onClick={this.props.click}>Create</button>
+          <input type="text" value={this.state.inputValue} onInput={this._handleInput} ref="description"/>
+          <p> {this.state.inputValue.length} / 80</p>
+          <button>Create</button>
         </form>
       </div>
     );
